ec45b719f4b59250eb8f386e783d76d01008ee17
Foucauld Degeorges <foucdeg@gmail.com>
2017-12-09 23:31:02 +0100

    Initial Commit

---
+++ PairProgram/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PairProgram
+//
+//  Created by Foucauld Degeorges on 09/12/2017.
+//  Copyright © 2017 Foucauld Degeorges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
